ecb902edfd6d5175870c028f3d09aad250f90b72
Gabriel  Alves <68339733+GabrielWeises@users.noreply.github.com>
2020-12-01 16:31:25 -0300

    Update conexao.php

--- lib/conexao.php
+++ lib/conexao.php
@@ -7,6 +7,6 @@ $USUARIO = $dbparts['wnw9m3go3dz7ymc6'];
 $SENHA = $dbparts['o7kvhj88jg2iob44'];
 $DB = ltrim($dbparts['mlvv59a1xr33ohk5'],'/');
 
-$conexao = new mysqliss($HOST, $USUARIO, $SENHA, $DB) or die ("não foi possivel conectar");
+$conexao = new mysqli($HOST, $USUARIO, $SENHA, $DB) or die ("não foi possivel conectar");
 
 ?>
