675cc25b9c69a0b7fe5c652a22f4a8bfec24a223
Gabriel  Alves <68339733+GabrielWeises@users.noreply.github.com>
2020-12-01 16:08:22 -0300

    Update conexao.php

--- lib/conexao.php
+++ lib/conexao.php
@@ -1,9 +1,11 @@
 <?php 
+$url = getenv('mysql://wnw9m3go3dz7ymc6:o7kvhj88jg2iob44@de1tmi3t63foh7fa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/mlvv59a1xr33ohk5');
+$dbparts = parse_url($url);
 
-define('HOST', 'localhost');
-define('USUARIO', 'root');
-define('SENHA', 'root');
-define('DB', 'usuario');
+define('HOST' = $dbparts['de1tmi3t63foh7fa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com']);
+define('USUARIO' = $dbparts['wnw9m3go3dz7ymc6']);
+define('SENHA' = $dbparts['o7kvhj88jg2iob44']);
+define('DB' = ltrim($dbparts['path'],'/'));
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ("não foi possivel conectar");
 
