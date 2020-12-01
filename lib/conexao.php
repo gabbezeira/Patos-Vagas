7d0fc5525a9458f34cff04d3625abf28d9fedb2f
Gabriel  Alves <68339733+GabrielWeises@users.noreply.github.com>
2020-12-01 14:43:09 -0300

    Curriculo Generation Base
    Base para o desenvolvimento do Curriculo Generation.

--- lib/conexao.php
+++ lib/conexao.php
@@ -1,9 +1,10 @@
 <?php 
 
-define('HOST', '127.0.0.1');
+define('HOST', 'localhost');
 define('USUARIO', 'root');
 define('SENHA', 'root');
 define('DB', 'usuario');
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ("não foi possivel conectar");
+
 ?>
---
+++ lib/curriculo_Gen/form.php
@@ -0,0 +1,38 @@
+<?php
+    include_once('tbs_class.php'); 
+    include_once('plugins/tbs_plugin_opentbs.php');
+
+    $TBS = new clsTinyButStrong; 
+    $TBS->Plugin(TBS_INSTALL, OPENTBS_PLUGIN); 
+
+    //Parametros
+
+    $title = 'Certificação';
+    $text = 'Certificado de conclusão';
+    $nome = 'Victor Michael';
+    $data = date('d,m,Y');
+    $logo = 'logo.jpg';
+
+    //carregando template
+    $template = 'teste.docx';
+    $TBS->LoadTemplate($template, OPENTBS_ALREADY_UTF8);
+    //Alterar Textos
+
+    $TBS->MergeField('pro.teste', $title);
+    $TBS->MergeField('pro.text', $text);
+    $TBS->MergeField('pro.nome', $nome);
+    $TBS->MergeField('pro.data', $data);
+    $TBS->VarRef['x'] = $logo;
+
+    $TBS->PlugIn(OPENTBS_DELETE_COMMENTS);
+
+    $save_as = (isset($_POST['save_as']) && (trim($_POST['save_as'])!=='') && ($_SERVER['SERVER_NAME']=='localhost')) ? trim($_POST['save_as']) : '';
+    $output_file_name = str_replace('.', '_'.date('Y-m-d').$save_as.'.', $template);
+    if ($save_as==='') {
+        $TBS->Show(OPENTBS_DOWNLOAD, $output_file_name); 
+        exit();
+    } else {
+        $TBS->Show(OPENTBS_FILE, $output_file_name);
+        exit("File [$output_file_name] has been created.");
+    }
+?>
