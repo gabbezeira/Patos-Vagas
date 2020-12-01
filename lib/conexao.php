0b3ab3f446b36f40003f5c28d70a1efbe6cbe2d9
Gabriel  Alves <68339733+GabrielWeises@users.noreply.github.com>
2020-12-01 16:38:20 -0300

    Update conexao.php

--- lib/conexao.php
+++ lib/conexao.php
@@ -5,7 +5,7 @@ $dbparts = parse_url($url);
 $HOST = $dbparts['de1tmi3t63foh7fa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'];
 $USUARIO = $dbparts['wnw9m3go3dz7ymc6'];
 $SENHA = $dbparts['o7kvhj88jg2iob44'];
-$DB = ltrim($dbparts['mlvv59a1xr33ohk5'],'/');
+$DB = ltrim($dbparts['mlvv59a1xr33ohk5']);
 
 $conexao = new mysqli($HOST, $USUARIO, $SENHA, $DB) or die ("não foi possivel conectar");
 
